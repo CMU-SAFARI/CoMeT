55bba8ccfd8d395cecd0ff1e7b9c18c81f93cd23
kanellok <kanellok@safari-sim0.ee.ethz.ch>
2023-11-10 17:51:40 +0100

    Added script to run the artifact without docker

---
+++ run_artifact_without_docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+print_colorful_text() {
+  local text="$1"
+  local color_code="$2"
+  echo -e "\e[${color_code}m${text}\e[0m"
+}
+
+if [ "$1" = "--slurm" ]; then
+  execution_mode_arg="--slurm"
+  echo "Running in job-based mode"
+elif [ "$1" = "--native" ]; then
+  execution_mode_arg="--native"
+  echo "Running in native mode"
+else 
+  echo "Provide correct execution mode: --slurm or --native"
+  exit
+fi
+
+# CREATE ASCII ART SAYING " CoMeT ARTIFACT"
+# Assuming you have an ASCII art generator command or script
+# ./generate_ascii_art " CoMeT ARTIFACT"
+
+echo "==================  Compiling the simulator =================="
+sh ./build.sh
+
+echo "==================  Generating run scripts =================="
+python3 ./genrunsp_cluster.py -wd $PWD -od results -td $PWD/cputraces
+
+# echo "==================  Decompressing the traces into ./cputraces =================="
+# wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=18BAvuQybyKT-RRHeAUFOsMAttG4xWlj-' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=18BAvuQybyKT-RRHeAUFOsMAttG4xWlj-" -O cputraces.tar.bz2 && rm -rf /tmp/cookies.txt
+
+# tar -xvf cputraces.tar.bz2
+
+# echo "==================  Launching experiments =================="
+# sh ./run.sh
