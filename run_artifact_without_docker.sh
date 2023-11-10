a8c89a58039c5b3d1a5e09bd9df494dd7e49ba90
kanellok <kanellok@safari-sim0.ee.ethz.ch>
2023-11-10 18:04:08 +0100

    Added support to run without docker without slurm

--- run_artifact_without_docker.sh
+++ run_artifact_without_docker.sh
@@ -7,10 +7,10 @@ print_colorful_text() {
 }
 
 if [ "$1" = "--slurm" ]; then
-  execution_mode_arg="--slurm"
+  execution_mode_arg="slurm"
   echo "Running in job-based mode"
 elif [ "$1" = "--native" ]; then
-  execution_mode_arg="--native"
+  execution_mode_arg="native"
   echo "Running in native mode"
 else 
   echo "Provide correct execution mode: --slurm or --native"
@@ -25,7 +25,7 @@ echo "==================  Compiling the simulator =================="
 sh ./build.sh
 
 echo "==================  Generating run scripts =================="
-python3 ./genrunsp_cluster.py -wd $PWD -od results -td $PWD/cputraces
+python3 ./genrunsp_cluster.py -wd $PWD -od results -td $PWD/cputraces --exec $execution_mode_arg
 
 # echo "==================  Decompressing the traces into ./cputraces =================="
 # wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=18BAvuQybyKT-RRHeAUFOsMAttG4xWlj-' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=18BAvuQybyKT-RRHeAUFOsMAttG4xWlj-" -O cputraces.tar.bz2 && rm -rf /tmp/cookies.txt
